23ad08811bb72601391b8dab2195097e3acfa7ba
Witek <milewskiwitek8@gmail.com>
2022-02-06 16:25:28 +0100

    feat(testportal): copy questions

--- src/content-scripts/testportal.js
+++ src/content-scripts/testportal.js
@@ -12,6 +12,33 @@
     return main.call(this, s);
   };
   document.hasFocus = () => true;
+
+
+  const questionElements = document.querySelectorAll('.question_essence > p')
+
+  if (!document.querySelectorAll('.quizit-p').length) {
+    const info = document.createElement('p')
+    info.textContent = "Click the question to copy it"
+    info.style.fontSize = "10px"
+    info.style.color = "#50C878"
+    info.style.marginTop = "-10px"
+    info.className = "quizit-p"
+    
+    if (questionElements.length)
+      questionElements[0].parentNode.append(info)
+  }
+
+  let questionText = ''
+  for (element of questionElements) {
+    questionText += ' ' + element.textContent
+  }
+
+  for (element of questionElements) {
+    element.style.cursor = 'pointer'
+    element.addEventListener("click", () => {
+      window.navigator.clipboard.writeText(questionText)
+    })
+  }
 `;
 
   document.body.appendChild(script);
