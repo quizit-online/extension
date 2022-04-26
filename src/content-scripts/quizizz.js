09eb706714d22a75e22ade338eac23734b36ba3c
nei7 <franciszek.szarek2@gmail.com>
2022-04-26 19:13:23 +0200

    fix: quizizz

--- src/content-scripts/quizizz.js
+++ src/content-scripts/quizizz.js
@@ -1,35 +1,29 @@
-(function () {
+function init() {
+  const script = document.createElement("script");
+  script.src =
+    "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js  ";
+
+  document.body.appendChild(script);
+
   const {
     game: { player, data },
     user,
   } = getCurrentData();
 
   if (!player.hasJoinedTheGame) {
-    alert("Can't current game data. Make sure you joined the game");
+    alert("Can't get current game data. Make sure you joined the game");
     return;
   }
 
-  const dataString = window.btoa(
-    unescape(
-      encodeURIComponent(
-        JSON.stringify({
-          playerId: player.playerId,
-          mongoId: user.profile.mongoId || null,
-          roomHash: data.roomHash,
-        })
-      )
-    )
-  );
-
   window.open(
-    "https://quizit.online/services/quizizz/answers?data=" + dataString
+    `https://quizit.online/services/quizizz/answers?playerId=${player.playerId}&game=${data.roomHash}&mongoId=${user.profile.mongoId}`
   );
-})();
+}
 
 function getCurrentData() {
   const root = document.querySelector("#root");
   if (!root) {
-    alert("Can't current game data. Make sure you joined the game");
+    alert("Can't get current game data. Make sure you joined the game");
     return;
   }
 
@@ -40,3 +34,5 @@ function getCurrentData() {
     user,
   };
 }
+
+(() => init())();
