bc82c4db70651756227745775cf3d44f536b4059
nei7 <franciszek.szarek2@gmail.com>
2022-04-26 18:50:53 +0200

    fix: quizizz

--- src/content-scripts/quizizz.js
+++ src/content-scripts/quizizz.js
@@ -1,15 +1,42 @@
-if (document.querySelector(".root-component")) {
-  const { game, user } = document.querySelector(".root-component").__vue__.$store.state;
-  const { player, data } = game;
+(function () {
+  const {
+    game: { player, data },
+    user,
+  } = getCurrentData();
 
-  if (player.hasJoinedTheGame) {
-    const dataString = window.btoa(
-      unescape(
-        encodeURIComponent(
-          JSON.stringify({ playerId: player.playerId, mongoId: user.profile.mongoId || null, roomHash: data.roomHash })
-        )
-      )
-    );
-    window.open("https://quizit.online/services/quizizz/answers?data=" + dataString);
+  if (!player.hasJoinedTheGame) {
+    alert("Can't current game data. Make sure you joined the game");
+    return;
   }
+
+  const dataString = window.btoa(
+    unescape(
+      encodeURIComponent(
+        JSON.stringify({
+          playerId: player.playerId,
+          mongoId: user.profile.mongoId || null,
+          roomHash: data.roomHash,
+        })
+      )
+    )
+  );
+
+  window.open(
+    "https://quizit.online/services/quizizz/answers?data=" + dataString
+  );
+})();
+
+function getCurrentData() {
+  const root = document.querySelector("#root");
+  if (!root) {
+    alert("Can't current game data. Make sure you joined the game");
+    return;
+  }
+
+  const { user, game } = root.__vue_app__.config.globalProperties.$store.state;
+
+  return {
+    game,
+    user,
+  };
 }
